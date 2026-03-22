914f4979aa5188ca189dcff55da50bdcfbad17ec
Ali <alialashwall14@gmail.com>
2026-03-22 03:24:56 +0200

    Optimizing task tabs by removing the redundant calculations

--- app/src/main/java/com/example/tudee/presentation/screens/tasks/TaskTabs.kt
+++ app/src/main/java/com/example/tudee/presentation/screens/tasks/TaskTabs.kt
@@ -1,49 +1,104 @@
 package com.example.tudee.presentation.screens.tasks
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.PrimaryTabRow
 import androidx.compose.material3.Tab
+import androidx.compose.material3.TabRowDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.tudee.database.entity.TasksEntity
+import com.example.tudee.presentation.components.TaskCard
 import com.example.tudee.presentation.designSystem.theme.Theme
+import com.example.tudee.presentation.screens.home.TaskStatus
 
 @Composable
 fun TaskTabs(
-    selectedTabIndex: Int = 0,
-    onTabClicked:(Int) -> Unit
+    selectedTab: TaskStatus,
+    statusTasksList: List<TasksEntity>,
+    onTabClicked: (TaskStatus) -> Unit,
+    modifier: Modifier = Modifier,
 ) {
-    val tabs = remember { listOf("In progress" to 14, "To Do" to null, "Done" to null) }
+    val tabs = TaskStatus.entries
 
-    PrimaryTabRow(selectedTabIndex = selectedTabIndex) {
+    // Calculate index once
+    val selectedTabIndex = remember(selectedTab) {
+        tabs.indexOf(selectedTab)
+    }
 
-        tabs.forEachIndexed { index, (title, count) ->
-            val textStyle =
-                if (selectedTabIndex == index) Theme.textStyle.label.medium else Theme.textStyle.label.small
-            Tab(
-                selected = selectedTabIndex == index,
-                onClick = {  onTabClicked(index) },
-                text = {
-                    Row(horizontalArrangement = Arrangement.spacedBy(6.dp)) {
-                        Text(
-                            text = title,
-                            color = if (selectedTabIndex == index) Theme.colors.title else Theme.colors.hint,
-                            style = textStyle
-                        )
-                        if (count != null) {
+    Column(modifier = modifier) {
+        PrimaryTabRow(
+            selectedTabIndex = selectedTabIndex,
+            indicator = {
+                TabRowDefaults.PrimaryIndicator(
+                    modifier = Modifier.tabIndicatorOffset(tabs.indexOf(selectedTab)),
+                    color = Theme.colors.secondary,
+                    width = 65.dp
+                )
+            }) {
+            tabs.forEachIndexed { index, taskStatus ->
+                val isSelected = index == selectedTabIndex
+                val textStyle =
+                    if (isSelected) Theme.textStyle.label.medium else Theme.textStyle.label.small
+                Tab(
+                    selected = isSelected,
+                    onClick = { onTabClicked(taskStatus) },
+                    text = {
+                        Row(horizontalArrangement = Arrangement.spacedBy(6.dp)) {
                             Text(
-                                text = count.toString(),
-                                color = if (selectedTabIndex == index) Theme.colors.body else Theme.colors.hint,
+                                text = taskStatus.label,
+                                color = if (isSelected) Theme.colors.title else Theme.colors.hint,
                                 style = textStyle
-
                             )
+                            if (isSelected && statusTasksList.isNotEmpty()) {
+                                Text(
+                                    text = statusTasksList.size.toString(),
+                                    color = Theme.colors.body,
+                                    style = textStyle
+                                )
+                            }
                         }
                     }
+                )
+            }
+        }
+
+        Spacer(Modifier.height(12.dp))
+
+        if (statusTasksList.isNotEmpty()) {
+            LazyColumn(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .weight(1f),
+                verticalArrangement = Arrangement.spacedBy(8.dp),
+            ) {
+                items(
+                    items = statusTasksList,
+                    key = { it.id }
+                ) { task ->
+                    TaskCard(
+                        taskIcon = task.categoryIcon,
+                        priorityLevel = task.priority,
+                        title = task.title,
+                        description = task.description,
+                        onClick = { },
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(horizontal = 16.dp)
+                    )
                 }
-            )
+            }
         }
     }
 }
@@ -53,7 +108,8 @@ fun TaskTabs(
 @Composable
 private fun TaskTabsPreview() {
     TaskTabs(
-        selectedTabIndex = 0,
-        onTabClicked = {}
+        selectedTab = TaskStatus.IN_PROGRESS,
+        onTabClicked = {},
+        statusTasksList = emptyList(),
     )
 }
